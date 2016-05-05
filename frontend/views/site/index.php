f0d3a394f8f158e3920ef59e0cd74194762b5cff
yothi <yothi@DESKTOP-9H2B322>
2016-05-05 18:44:19 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,6 +1,25 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = 'ผู้มารับบริการแม่และเด็ก@รพ.มุกดาหาร';
 ?>
+
+<?php
+
+$route1=Yii::$app->UrlManager->createUrl('test/test1');
+
+?>
+<a href="<?=$route1;?>">ไปที่ test1</a> = ทำ link url ธรรมดา ไปแสดงที่ view<br>
+
+<?php
+
+$route2=Yii::$app->UrlManager->createUrl(['test/test2','name'=>'Yothin2','lname'=>'Traiyawong2']);
+
+?>
+<a href="<?=$route2;?>">ไปที่ test2 </a> = ส่งค่าไป controller แล้วไปแสดงที่ view<br>
+
+<?=
+Html::a('ลิงค์แบบที่3', ['test/test1','a'=>'1']);
+
+?> = ทำ link url ธรรมดา ไปแสดงที่ view style Yii <br>
